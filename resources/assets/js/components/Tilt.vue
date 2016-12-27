05b4654631d2ac4580c82308cfafcba9c85a90b9
dkuck <dkuck@thatsus.com>
2016-12-27 10:40:51 -0500

    Use the boardwalker

--- resources/assets/js/components/Tilt.vue
+++ resources/assets/js/components/Tilt.vue
@@ -4,16 +4,56 @@
 			:board="startBoard"
 			>
 		</board>
+
+	    <div class="controls">
+	      <button @click="reset">Reset</button>
+	      <span v-if="done" class="badge">Done</span>
+	      <template v-else>
+		      <button v-if="running" @click="stop">Stop</button>
+		      <button v-else @click="run">Run</button>
+		      <button @click="step">Step</button>
+		  </template>
+	    </div>
 	</div>
 </template>
 
 <script>
 import Maker from '../Maker.js';
+import BoardWalker from '../BoardWalker.js';
 export default {
 	data() {
+		var board = Maker.generateRandomBoard();
 		return {
-			startBoard: Maker.generateRandomBoard()
+			startBoard: board,
+		    walker: new BoardWalker(board),
+		    running: null,
 		};
+	},
+	computed: {
+		done() {
+			return this.walker.active.length === 0;
+		},
+	},
+	methods: {
+		reset() {
+			if (this.running) {
+				this.stop();
+			}
+			this.startBoard = Maker.generateRandomBoard();
+		    this.walker = new BoardWalker(this.startBoard);
+		},
+		step() {
+			this.walker.step();
+		},
+		run() {
+			this.running = setInterval(() => {
+				this.step();
+			}, 100);
+		},
+		stop() {
+			clearInterval(this.running);
+			this.running = null;
+		}
 	}
 }
 </script>
