ba5077cdab91e41e9a716ffc00a167b856b12b2e
dkuck <dkuck@thatsus.com>
2017-01-06 16:33:21 -0500

    Paginate the lists of paths

--- resources/assets/js/components/Paths.vue
+++ resources/assets/js/components/Paths.vue
@@ -1,7 +1,14 @@
 <template>
 <div>
+	<div v-if="pageCount > 1">
+        <div class="tabs">
+            <div v-for="page in pages" class="tab" :class="tabClass(page)" @click="showPage(page)">
+            	{{ page }}
+            </div>
+        </div>
+	</div>
 	<div v-if="paths.length > 0">
-		<div v-for="path in paths">
+		<div v-for="path in pagedPaths">
 			<board-path :path="path"></board-path>
 		</div>
 	</div>
@@ -14,5 +21,37 @@
 <script>
 export default {
 	props: ['paths', 'noneMessage'],
+	data() {
+		return {
+			page: 1,
+			pageLength: 10,
+		};
+	},
+	computed: {
+		pagedPaths() {
+			return this.paths.slice((this.page - 1) * this.pageLength, this.page * this.pageLength);
+		},
+		pageCount() {
+			return Math.ceil(this.paths.length / this.pageLength);
+		},
+		pages() {
+			var pages = [];
+			for (var i = 1; i <= this.pageCount; i++) {
+				pages.push(i);
+			}
+			return pages;
+		},
+	},
+	methods: {
+        showPage(page) {
+            this.page = page;
+        },
+        tabClass(page) {
+            if (page === this.page) {
+                return "active";
+            }
+            return "";
+        },
+    },
 }
 </script>
